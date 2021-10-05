26c3ac4acb1b7e37b47dc98a70e5defe0e6cacf6
michael2708 <akomolafem@gmail.com>
2021-10-05 10:51:40 -0700

    adds mainh

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+/**
+ * struct types - struct
+ * @id: identifier of type to print (e.g. c means char)
+ * @f: ptr to functions that print according to identifier (e.g. print_c)
+ */
+
+typedef struct types
+{
+	char *id;
+	int* (*f)(va_list args);
+} print;
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
